5d1584bfd4b3fe5c177e6213a2cc3cfa101c687f
Brock Young <redsalmonfrog@gmail.com>
2025-03-10 14:54:31 -0700

    add consultant without toString

---
+++ A3_Starter/Consultant.java
@@ -0,0 +1,13 @@
+public class Consultant extends Manager {
+    public Consultant(String name, int ssn, String employeeID) {
+        super(name, ssn, employeeID);
+    }
+
+    public String toString() {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
